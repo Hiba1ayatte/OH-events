fb8696ad97254d498ad34ba58023ad154487a0f4
= <hiba.ayatte123@gmail.com>
2022-12-12 15:27:13 +0100

    gender

---
+++ gender.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gender</title>
+
+    <link rel="stylesheet" href="css/style.css"/>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+</head>
+<body>
+<div class="container">
+
+<section class="header">
+
+   <a href="home.php" class="logo">OH Events</a>
+
+   <nav class="navbar">
+      <a href="portfolio.php">Nos Designs</a>
+      <a href="contact.php"> Se Connecter</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<section class="local">
+<h1 class="heading">Féte De Révalution</h1>
+
+<div class="swiper local-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img src="images_site/gender/gender1.jfif" alt="">
+         <div class="content">
+            <a href="contact.php" class="btn">demander</a>
+         </div>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images_site/gender/gender2.jfif" alt="">
+         <div class="content">
+            <a href="contact.php" class="btn">demander</a>
+         </div>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images_site/gender/gender3.jfif" alt="">
+         <div class="content">
+            <a href="contact.php" class="btn">demander</a>
+         </div>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images_site/gender/gender4.jfif" alt="">
+         <div class="content">
+            <a href="contact.php" class="btn">demander</a>
+         </div>
+      </div>
+   </div>
+</div>   
+     
+<div class="swiper local-slider">
+      <div class="swiper-wrapper">
+      <div class="swiper-slide slide">
+         <img src="images_site/gender/gender5.jfif" alt="">
+         <div class="content">
+            <a href="contact.php" class="btn">demander</a>
+         </div>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images_site/gender/gender6.jfif" alt="">
+         <div class="content">
+            <a href="contact.php" class="btn">demander</a>
+         </div>
+      </div>
+       
+      <div class="swiper-slide slide">
+         <img src="images_site/gender/gender7.jfif" alt="">
+         <div class="content">
+            <a href="contact.php" class="btn">demander</a>
+         </div>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images_site/gender/gender8.jfif" alt="">
+         <div class="content">
+            <a href="contact.php" class="btn">demander</a>
+         </div>
+      </div>
+      </div>
+     
+</div>
+</section>
+
+<?php @include 'footer.php'; ?>
+
+</div>
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
