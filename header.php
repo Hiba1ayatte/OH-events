83221cac9131ceeefca71eb0ee2fe721d608abfd
= <hiba.ayatte123@gmail.com>
2023-01-13 15:27:00 +0100

    y

--- header.php
+++ header.php
@@ -8,6 +8,11 @@
       <a href="portfolio.php">Nos Designs</a>
       <a href="pricing.php">Prix</a>
       <a href="contact.php"> Se Connecter</a>
+      <!-- <form action="" id="search-form">
+        <input type="search" placeholder="search here..." name="" id="search-box">
+        <label for="search-box" class="fas fa-search"></label>
+        <i class="fas fa-times" id="close"></i>
+        </form> -->
    </nav>
 
    <div id="menu-btn" class="fas fa-bars"></div>
